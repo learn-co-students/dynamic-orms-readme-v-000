e734deb7b0ac42c906a40445d0c1a4c703eb8e1a
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-13 11:50:32 -0500

    finished readme, need to refactor

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
 
   def self.column_names
     DB[:conn].results_as_hash = true
+
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
@@ -26,19 +27,26 @@ class Song
 
   def initialize(options={})
     options.each do |property, value|
-      send("#{property}=", value)
+      self.send("#{property}=", value)
     end
   end
 
   def save
-    sql = "INSERT INTO #{self.class.table_name} (#{col_names_for_insert}) VALUES (#{values_for_insert.join(", ")})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
-    self.class.column_names.map do |col_name|
-      "'#{public_send(col_name)}'" unless public_send(col_name).nil?
-    end.compact
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
 
   def col_names_for_insert
