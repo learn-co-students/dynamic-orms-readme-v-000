ade0309e4e6bd9bb75e82b7e767cb482037356e6
marcotsui2003 <marcotsui2003@yahoo.com>
2016-01-22 20:50:55 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
+require 'active_support/inflector' #for pluralize
 
 class Song
 
@@ -11,7 +11,7 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "pragma table_info(#{table_name})"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
