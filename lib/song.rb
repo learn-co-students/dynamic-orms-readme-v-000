11faa021c9b008cc798f22c5da4b7ae807020381
Chris Bruen <bruen.chris@gmail.com>
2016-01-31 16:05:29 -0500

    done

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
+      values<< "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
   end
