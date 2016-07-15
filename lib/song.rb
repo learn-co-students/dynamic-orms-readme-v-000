9b6255312236f6b7a84a4759e7c67f4626537e4b
Rebecca Sherman <beckysherman000@gmail.com>
2016-07-15 21:53:59 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,17 @@ require 'active_support/inflector'
 
 class Song
 
-
   def self.table_name
+    # Returns the name of a table, given the name of a class
     self.to_s.downcase.pluralize
   end
 
   def self.column_names
+    # Queries table for names of its columns
+    # Returns array of hashes describing table
+    # Each hash has info about one column
+    # Iterate over the resulting array of hashes to collect just the name of each column
+    # Return value will be ["id", "name", "album"]
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
@@ -18,14 +23,18 @@ class Song
     table_info.each do |row|
       column_names << row["name"]
     end
+    # Calling .compact removes any nil values from collection
+    # This collection is used to create attr_accessors for Song class
     column_names.compact
   end
 
   self.column_names.each do |col_name|
+    # Creates attr_accessors using column names; converts to symbols
     attr_accessor col_name.to_sym
   end
 
   def initialize(options={})
+    # Builds dynamic initialize method
     options.each do |property, value|
       self.send("#{property}=", value)
     end
@@ -57,8 +66,4 @@ class Song
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
-
 end
-
-
-
