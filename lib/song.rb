c64a713184a4a90c4393e1f2a5c35e6692217fef
Dalma Boros <dalma.boros@gmail.com>
2017-04-03 13:54:33 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,14 @@ class Song
 
     sql = "pragma table_info('#{table_name}')"
 
-    table_info = DB[:conn].execute(sql)
+    table_info = DB[:conn].execute(sql) #table_info is an array of hashes
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+    # binding.pry
+    table_info.each do |row| #row is a hash corresponding to a column
+      column_names << row["name"] #["name"] is the key corresponding to the column's name
     end
-    column_names.compact
+    # binding.pry
+    column_names.compact #compact removes nil elements
   end
 
   self.column_names.each do |col_name|
@@ -25,15 +27,18 @@ class Song
   end
 
   def initialize(options={})
+    # binding.pry
     options.each do |property, value|
       self.send("#{property}=", value)
     end
+    # binding.pry
   end
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    # binding.pry
   end
 
   def table_name_for_insert
@@ -44,12 +49,14 @@ class Song
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+    end #returns array of strings equal to column names
+    # binding.pry
+    values.join(", ") #joins elements into single string, each separated by ", "
   end
 
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    #column_names returns array, this method joins elements into single string
   end
 
   def self.find_by_name(name)
