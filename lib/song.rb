dd1e6ee29e7e17d3f7ec2886dfc196c064d80754
gb23 <gregbenj@hotmail.com>
2017-06-20 16:35:49 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,12 +20,13 @@ class Song
     end
     column_names.compact
   end
-
+binding.pry
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
   def initialize(options={})
+
     options.each do |property, value|
       self.send("#{property}=", value)
     end
