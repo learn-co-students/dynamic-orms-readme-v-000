512ebdfde1561e33db0d73ba9c33eff54216f1e9
Hannah Carnes <hecarnes@gmail.com>
2017-12-31 17:51:49 -0500

    done

--- lib/song.rb
+++ lib/song.rb
@@ -3,30 +3,43 @@ require 'active_support/inflector'
 
 class Song
 
-
+  #changes Song to songs
   def self.table_name
     self.to_s.downcase.pluralize
   end
-
+ 
+  #gets column names
   def self.column_names
+  #when a SELECT statement is executed, 
+  #don't return a database row as an array, return it as a 
+  #hash with the column names as keys.
     DB[:conn].results_as_hash = true
-
+    #query table for names of columns & will return an array of hashes with column info
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
+    #iterate over the array of hashes to get just the name of each column
     table_info.each do |row|
       column_names << row["name"]
     end
+    #.compact will get rid of any nil values
     column_names.compact
   end
 
+  #iterate over the column names stored in the column_names class method 
+  #and set an attr_accessor for each one
   self.column_names.each do |col_name|
+  #convert the column name string into a symbol with the #to_sym method, 
+  #since attr_accessors must be named with symbols.
     attr_accessor col_name.to_sym
   end
 
+  #takes in an argument of options, which is an empty hash 
   def initialize(options={})
     options.each do |property, value|
+  #send method to interpolate the name of each hash key as a method 
+  #that we set = to that key's value
       self.send("#{property}=", value)
     end
   end
