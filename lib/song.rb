26b23adea510778373c3559a92d470e20159d5ed
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-25 06:01:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,9 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class Song
+require 'pry'
 
+class Song
 
   def self.table_name
     self.to_s.downcase.pluralize
@@ -14,11 +15,7 @@ class Song
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
+    table_info.map { |row| row["name"] }.compact
   end
 
   self.column_names.each do |col_name|
@@ -32,9 +29,11 @@ class Song
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+      VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid()
+      FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
@@ -42,11 +41,9 @@ class Song
   end
 
   def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+    self.class.column_names.map do |col_name|
+      "'#{send(col_name)}'" unless send(col_name).nil?
+    end.compact.join(", ")
   end
 
   def col_names_for_insert
@@ -64,6 +61,3 @@ class Song
   end
 
 end
-
-
-
