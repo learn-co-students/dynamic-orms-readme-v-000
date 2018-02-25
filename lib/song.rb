414e6994bfa48a8849161877f01b1e9b4575f74b
Robert Hughes <roberts.ginger.email@gmail.com>
2018-02-25 05:33:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -58,6 +58,11 @@ class Song
     DB[:conn].execute(sql)
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = #{name}"
+    DB[:conn].execute(sql)
+  end
+
 end
 
 
