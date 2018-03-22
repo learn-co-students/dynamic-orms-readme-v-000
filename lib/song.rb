3c6ce6a3e1368e46c4e1d938f8647c5cb165c603
Jul Jen <juliajen.prog@gmail.com>
2018-03-22 19:16:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 
 class Song
 
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -11,10 +10,11 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
+
     table_info.each do |row|
       column_names << row["name"]
     end
@@ -59,6 +59,3 @@ class Song
   end
 
 end
-
-
-
