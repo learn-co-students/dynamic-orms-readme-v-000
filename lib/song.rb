0b7d9d31b665dff821481d2248d5c83ed5c9b828
Christopher Wood <cjwood032@gmail.com>
2018-07-13 18:11:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,8 @@ class Song
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+    table_info.each do |column|
+      column_names << column["name"]
     end
     column_names.compact
   end
