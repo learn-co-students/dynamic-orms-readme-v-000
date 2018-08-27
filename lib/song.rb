dea3da58b1227c563793dc8f7af915c48c7c291c
DMCatanach <dawn.catanach@gmail.com>
2018-08-27 17:00:20 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
+require 'active_support/inflector' #provides #pluralize used in #table_name
 
 class Song
 
@@ -11,23 +11,23 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
     end
-    column_names.compact
+    column_names.compact #make sure there aren't any nil values among our table names 
   end
 
   self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
+    attr_accessor col_name.to_sym #this is writing code for us 
   end
 
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
+  def initialize(options={}) #takes in hash of keyword arguments, default is empty hash, new should be called on hash (from table)
+    options.each do |property, value| #this is also writing code for us
+      self.send("#{property}=", value)  #we're sending the setter method to our receiver; setter will change with the property name
     end
   end
 
@@ -38,19 +38,19 @@ class Song
   end
 
   def table_name_for_insert
-    self.class.table_name
+    self.class.table_name #self would refer to instance, so class added to pop up one level to the class method
   end
 
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
+      values << "'#{send(col_name)}'" unless send(col_name).nil? #sending the getter for each column, returns attribute of that getter
     end
-    values.join(", ")
+    values.join(", ") #puts returned values together in comma-separated list
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ") #don't insert id into the table; that'll be handled by the db
   end
 
   def self.find_by_name(name)
