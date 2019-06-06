9304c89de57b29eab8b04cf882af616211cd058a
Tim Knoll <timothy.knoll@gmail.com>
2019-06-05 22:54:33 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,14 @@
 require 'sqlite3'
 
+#STEP 1
 
+# creating database
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
+
+#drop songs to avoid error
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
 
+#creating songs table
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -13,4 +18,7 @@ sql = <<-SQL
 SQL
 
 DB[:conn].execute(sql)
+
+#when SELECT statement is executed don't return a database row as an array, 
+#return it as has with column names as keys
 DB[:conn].results_as_hash = true
--- lib/song.rb
+++ lib/song.rb
@@ -3,56 +3,87 @@ require 'active_support/inflector'
 
 class Song
 
+  #STEP 2
+  #use the songs table column names to dynamically create the attr_accessor's of Song class
+    #need to query for columns names  
 
-  def self.table_name
+  def self.table_name #class method that grabs table name
     self.to_s.downcase.pluralize
   end
 
-  def self.column_names
+  def self.column_names #class method that grabs columns names
+    #queries table for column names
     DB[:conn].results_as_hash = true
-
     sql = "pragma table_info('#{table_name}')"
 
+    #iterate over query just collecting column names
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
     end
-    column_names.compact
+    column_names.compact #compact get rids of nil values
   end
 
-  self.column_names.each do |col_name|
+  # iterate of column names stored in column_names class method & 
+  # set an attr_accessor for each one
+  self.column_names.each do |col_name| 
     attr_accessor col_name.to_sym
   end
 
-  def initialize(options={})
+
+  #STEP 3 
+  #builing an abstract initialize method
+
+  def initialize(options={})  #method take in an agrument options = empty hash
     options.each do |property, value|
-      self.send("#{property}=", value)
+      self.send("#{property}=", value) #.send interpolate the name of each hash as a method that we set equals to key's value
     end
   end
 
+  #STEP 4
+  #writing ORM (object relational mapping) methods save and find_by_name
+
   def save
+    # INSERT INTO songs (name, album) VALUES ?, ?
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
     DB[:conn].execute(sql)
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  #def self.xxxx   = class method & self refers to class Song
+  #end 
+
+  #def xxxx  = instance method 
+    #self.xxxx   => self refers to instance xxxx of the class Song 
+    #self.class.xxxx  => self.class gives status class and can call arguments on the class
+  #end  
+  
   def table_name_for_insert
     self.class.table_name
   end
 
+  def col_names_for_insert
+    #self.class.column_names returns ["id", "name", "album"]
+    
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ") 
+    # returns ["name", "album"]  removes "id" from array
+    # then joins "name, album"
+  end
+
   def values_for_insert
     values = []
+ 
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
 
+#select rocords 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
